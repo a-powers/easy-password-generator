cc27b0607613edee04a7315b2dffe31419081227
a-powers <awpowers@gmail.com>
2021-02-16 12:59:41 +0000

    update

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 print("Welcome to the Super Safe Password Generator!")
 print("Follow the prompts to make your password simple or complex.")
 print("")
+
+
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
@@ -20,6 +22,7 @@ for char in range(1, nr_symbols + 1):
 
 for char in range(1, nr_numbers + 1):
     random_chars += random.choice(numbers)
+
 
 random.shuffle(random_chars)
 
